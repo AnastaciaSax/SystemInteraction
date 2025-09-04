4db9a50a2d8fcb46f6e1ebddb4c631dfd8ef0d8d
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-04 20:02:25 +0300

    feat: structure complete

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from "react"; 
 import './App.css';
 import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Header"; 
+import Title from "./components/Title";
+import Agency from "./components/Agency";
+import SectionService from "./components/Section-service";
+import SectionAbout from "./components/Section-about-us";
+import SectionWork from "./components/Section-our-work";
+import SectionTest from "./components/Section-testimonial";
+import SectionProject from "./components/Section-have-project";
+import Footer from "./components/Footer";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <div className="App">
+        <div className="thumb">
+        <Header />
+        <Title />
+        </div>
+        <div className="web">
+        <Agency />
+         <SectionService />
+         <SectionAbout />
+         <SectionWork />
+         <SectionTest />
+         <SectionProject />
+         <Footer />
+        </div>
+
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
