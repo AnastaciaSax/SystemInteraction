fea51a5daee2f081e5f1beb2676c83111b50f78b
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-19 13:19:58 +0300

    feat: pages applied

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// page
+import Home from "./pages/home";
+import Catalog from "./pages/catalog";
+import ItemDetails from "./pages/ItemDetails"; 
+//import Cart from "./pages/cart";
+
 function App() {
   return (
-    <div>
-      <h1>My React SPA</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/catalog/:id" element={<ItemDetails />} /> 
+        {/* <Route path="/cart" element={<Cart />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
