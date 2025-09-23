23fdc63112a9591e2efc57562124f68ff96a9ba1
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-23 20:42:52 +0300

    feat: miu catalog banner

--- AtalogBanner.jsx
+++ AtalogBanner.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 function CatalogBanner() {
   return (
-    <main className="banner banner--catalog">
-      <div className="intro">
-        <div className="into-title">
-          <h2 data-i18n="catalogTitle">Design Catalog</h2>
-          <span data-i18n="homeCatalogBreadcrumb">Home / Catalog</span>
-        </div>
-      </div>
-    </main>
+    <Box component="main" className="banner banner--catalog">
+      <Box className="intro">
+        <Box className="into-title">
+          <Typography component="h2" data-i18n="catalogTitle" sx={{
+    color: "#292F36",
+    fontFamily: '"DM Serif Display", serif',
+    fontWeight: 400,
+    lineHeight: "125%",
+     fontSize: "50px",
+     whiteSpace: "nowrap"
+  }}>
+            
+            Design Catalog
+          </Typography>
+          <Typography component="span" data-i18n="homeCatalogBreadcrumb">
+            Home / Catalog
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
