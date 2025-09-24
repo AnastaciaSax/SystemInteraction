e984d1120f0bffb53201402c06156274400b2f53
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-24 11:35:17 +0300

    feat: miu item banner

--- ItemBanner.jsx
+++ ItemBanner.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const ItemBanner = ({ title }) => (
-  <main className="banner  banner--item">
-    <div className="intro">
-      <div className="into-title">
-        <h2>{title}</h2>
-        <span>Home / Catalog / {title}</span>
-      </div>
-    </div>
-  </main>
+  <Box component="main" className="banner banner--item">
+    <Box className="intro">
+      <Box className="into-title">
+        <Typography 
+  variant="h2" 
+  sx={{ fontSize: "50px", fontFamily: '"DM Serif Display", serif', fontWeight: 400, lineHeight: "125%", whiteSpace:"nowrap" }}
+>
+  {title}
+</Typography>
+
+<Typography 
+  component="span" 
+  variant="body1" 
+  sx={{ fontSize: "22px", whiteSpace:"nowrap" }}
+>
+  Home / Catalog / {title}
+</Typography>
+      </Box>
+    </Box>
+  </Box>
 );
 
 export default ItemBanner;
