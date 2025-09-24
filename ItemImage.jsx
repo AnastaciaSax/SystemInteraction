6a097df18a2e095aea9c8e64431b900ae867f7ab
AnastaciaSax <nastya.saks2016@yandex.com>
2025-09-24 11:36:16 +0300

    feat: miu item image

--- ItemImage.jsx
+++ ItemImage.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Box, CardMedia } from "@mui/material";
 
 const ItemImage = ({ src, alt }) => (
-  <div className="details-pic">
-    <img src={src} alt={alt} />
-  </div>
+  <Box
+    className="details-pic"
+    sx={{
+      flex: 2,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <CardMedia
+      component="img"
+      image={src}
+      alt={alt}
+      sx={{
+        maxWidth: "100%",
+        borderRadius: "12px",
+        boxShadow: "0 8px 24px rgba(0, 0, 0, 0.15)",
+      }}
+    />
+  </Box>
 );
 
 export default ItemImage;
