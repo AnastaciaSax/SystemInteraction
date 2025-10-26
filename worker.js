a8708e5892b4fd55ccdc99585f26233fa4958e73
AnastaciaSax <nastya.saks2016@yandex.com>
2025-10-26 09:17:24 +0300

    fix: filter

--- worker.js
+++ worker.js
@@ -1,15 +1,27 @@
-
 import { parentPort, workerData } from "worker_threads";
 import fs from "fs";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from 'url';
 
 const { keyword } = workerData;
 
-const indexPath = resolve("data", "service_index.json");
+// Получаем директорию текущего файла
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Путь к index
+const indexPath = resolve(__dirname, "../data/service_index.json");
+
+// Чтение данных
 const indexData = JSON.parse(fs.readFileSync(indexPath, "utf8"));
 
+// Фильтруем по ключевому слову
 const results = indexData.services.filter(s =>
-  s.title.toLowerCase().includes(keyword.toLowerCase())
+  s.title.toLowerCase().includes(keyword.toLowerCase()) ||
+  s.place.toLowerCase().includes(keyword.toLowerCase())
 );
 
+// Отправляем результат обратно в главный поток
 parentPort.postMessage(results);
+
+
