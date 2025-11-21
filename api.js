9be0834298638911c9a2a3f8c71492066bc23e01
AnastaciaSax <nastya.saks2016@yandex.com>
2025-11-21 08:31:21 +0300

    file: api added

---
+++ api.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const API_BASE_URL = '/api';
+
+// Создаем экземпляр axios с базовым URL
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Интерцептор для обработки ошибок
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API Error:', error);
+    return Promise.reject(error);
+  }
+);
+
+// API для стран
+export const countriesAPI = {
+  getAll: (params = {}) => api.get('/countries', { params }),
+  getById: (id) => api.get(`/countries/${id}`),
+  create: (data) => api.post('/countries', data),
+  update: (id, data) => api.put(`/countries/${id}`, data),
+  delete: (id) => api.delete(`/countries/${id}`),
+};
+
+// API для маршрутов
+export const routesAPI = {
+  getAll: (params = {}) => api.get('/routes', { params }),
+  getById: (id) => api.get(`/routes/${id}`),
+  create: (data) => api.post('/routes', data),
+  update: (id, data) => api.put(`/routes/${id}`, data),
+  delete: (id) => api.delete(`/routes/${id}`),
+};
+
+// API для продаж
+export const salesAPI = {
+  getAll: (params = {}) => api.get('/sales', { params }),
+  getStats: (params = {}) => api.get('/sales/stats', { params }),
+  getById: (id) => api.get(`/sales/${id}`),
+  create: (data) => api.post('/sales', data),
+  update: (id, data) => api.put(`/sales/${id}`, data),
+  delete: (id) => api.delete(`/sales/${id}`),
+};
+
+export default api;
